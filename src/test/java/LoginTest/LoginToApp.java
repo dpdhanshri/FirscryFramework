013f8263e587a894a617ba0170e2b42241918de2
Dhanashri <dpdhanashri@gmai.com>
2018-12-17 15:36:39 +0530

    chnaged

--- src/test/java/LoginTest/LoginToApp.java
+++ src/test/java/LoginTest/LoginToApp.java
@@ -1,12 +1,10 @@
 package LoginTest;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import LoginModule.LoginLocators;
+import common.LoginLocators;
 import common.ReadData;
 import common.WebDriverUtils;
 
@@ -16,27 +14,33 @@ public class LoginToApp extends LoginLocators
 	String invalidUsername = "1234567890";
 	String invalidPassword = "password12345";
 
+	@BeforeClass
+	public void setup()
+	{
+		WebDriverUtils.loadBrowser();
+		pause(2000);
+		WebDriverUtils.openApplication(URL);
+		pause(1000);
+		popupHandle();	
+	}
+
 	public void login(String userName, String password)
 	{
-		popupHandle();		
 		click(LoginLocators.Login);
 		pause(2000);
 		typeText(LoginLocators.Email,userName);
 		typeText(LoginLocators.pass,password);
-		pause(2000);
+		pause(1000);
 		mouseMove();
 		click(LoginLocators.LoginButton);
 
 	}
-
-	@BeforeClass
-	public void setup()
-
+	@Test(priority =0)
+	public void validlogin()
 	{
-		WebDriverUtils.loadBrowser();
-		pause(3000);
-		WebDriverUtils.openApplication(URL);
-		
+		ReadData.readCredentials();
+		pause(1000);
+		login(ReadData.MyUser,ReadData.pass);
 	}
 
 	//*@Test
@@ -45,13 +49,6 @@ public class LoginToApp extends LoginLocators
 		login(invalidUsername,invalidPassword);
 		Assert.assertEquals(getInvalidLoginMessage(), "Please enter valid Email ID/Mobile number");
 
-	}
-
-	@Test
-	public void validlogin()
-	{
-		ReadData.readCredentials();
-		login(ReadData.MyUser,ReadData.pass);
 	}
 
 	//@AfterClass
--- src/test/java/common/LoginLocators.java
+++ src/test/java/common/LoginLocators.java
@@ -1,19 +1,10 @@
-package LoginModule;
+package common;
 
 import org.openqa.selenium.By;
 
-import common.PopUpCommonHandle;
-
-public class LoginLocators extends PopUpCommonHandle
-{
-	
+public class LoginLocators extends PopUpCommonHandle {
 	protected static By Login = By.xpath("//span[@class='poplogin_main poplogin']");
 	protected static By Email = By.xpath("//input[@id='txtLUNm']");
 	protected static By pass = By.xpath("//input[@id='txtLPass']");
 	protected static By LoginButton = By.xpath("//input[@id='btnlogon']");
-			
-   //search module locators
-	protected static By Search = By.xpath("//input[@class ='input-text']");
-	protected static By SearchIcon = By.xpath("//img[@class ='search-button']");
 }
-
