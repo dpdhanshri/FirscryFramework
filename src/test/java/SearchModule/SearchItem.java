1f3952d030f80388270af1116d4d46da933c4e0c
Dhanashri <dpdhanashri@gmai.com>
2018-12-27 16:34:14 +0530

    chng

--- src/test/java/SearchModule/SearchItem.java
+++ src/test/java/SearchModule/SearchItem.java
@@ -1,19 +1,23 @@
 package SearchModule;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import common.LoginLocators;
 import common.ReadData;
 import common.SearchLocators;
+import common.WebDriverUtils;
 
 public class SearchItem extends SearchLocators
 
 {
-	@Test(priority =1)
+	@Test()
 	public void SearchMyItem() throws InterruptedException
 	{
-		pause(1000);
 		click(SearchLocators.Search);
-		typeText(SearchLocators.Search, ReadData.Item1);
+		System.out.println("hi");
+		typeText(SearchLocators.Search,ReadData.Item1);
 		pause(1000);
 		click(SearchLocators.SearchIcon);
 		Thread.sleep(10000);
